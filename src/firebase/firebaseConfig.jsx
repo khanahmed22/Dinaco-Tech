beb8a82e3d8072424af1fca72233ab6722034c52
Muhammad Ahmed Khan <ahmed.mak26@outlook.com>
2024-04-30 20:35:19 +0500

    Update firebaseConfig.jsx

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjGNzwUlr8lFd97NUV6QD1oN0TlPf1T3k",
-  authDomain: "ecommerce-e9787.firebaseapp.com",
-  projectId: "ecommerce-e9787",
-  storageBucket: "ecommerce-e9787.appspot.com",
-  messagingSenderId: "492613754484",
-  appId: "1:492613754484:web:0868fa6edb3bd82fc5fb21",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
